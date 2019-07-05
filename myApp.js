602bcb2d07e101a5dc564022970329287544b7a9
Robert Carrier <robmcarrier@gmail.com>
2019-07-04 17:27:37 -0700

    Create routers

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
-var express = require('express');
-var ds = require('./dateModule.js');
-var JobApplication = require('./models/jobApplication');
-var mongoose = require('mongoose');
+var express = require('express'),
+    ds = require('./dateModule.js'),
+    JobApplication = require('./models/jobApplication'),
+    mongoose = require('mongoose'),
+    createError = require('http-errors'),
+    path = require('path'),
+    indexRouter = require('./routes/index'),
+    jobApplicationRouter = require('./routes/jobApplications');
 
 var app = express();
 var mongoDB = 'mongodb://127.0.0.1/my_database';
@@ -11,19 +15,20 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-JobApplication.create({company: 'test', jobTitle: 'Test Position', status: "Submitted"},
-    function(err, jobApplication) {
-        if (err) return new Error(err);
-    });
+// JobApplication.create({company: 'test', jobTitle: 'Test Position', status: "Submitted"},
+//     function (err, jobApplication) {
+//         if (err) return new Error(err);
+//     });
 
 JobApplication.findOne({}).exec(function (err, jobApplications) {
-    if (err) return new Error(err);
+    if (err) createError(err);
     console.log(jobApplications.jobTitle);
 });
 
-const myLogger = function(req, res, next) {
+const myLogger = function (req, res, next) {
     console.log("Request IP: " + req.ip);
     console.log("Request Method: " + req.method);
     console.log("Request date: " + ds.myDateTime());
@@ -32,20 +37,10 @@ const myLogger = function(req, res, next) {
 };
 
 app.use(myLogger);
+app.use('/', indexRouter);
+app.use('/jobApplications', jobApplicationRouter);
 
-app.get('/', function(req, res) {
-    res.render('pages/index')
-});
-
-app.get('/about', function(req, res) {
-    res.render('pages/about')
-});
-
-app.get('/contact-me', function(req, res) {
-    res.render('pages/contact-me')
-});
-
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
     res.render('pages/404')
 });
 
